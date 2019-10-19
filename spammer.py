081e46dfd1c8d866e3dfbc950cda6ddfaa836c42
MGM3199 <48875764+MGM3199@users.noreply.github.com>
2019-10-19 23:59:05 +0700

    Create spammer.py

---
+++ spammer.py
@@ -0,0 +1,12 @@
+# ! / usr / bin / python
+
+# - Spammer v2
+# | Deskripsi: Memalsukan nomor telepon dengan mengirimkannya banyak sms menggunakan Grab API
+# | Penulis: P4kL0nc4t
+# | Tanggal: 5/12/2017
+# | Penafian: Penulis pengeditan tidak akan menjadikan Anda pembuat kode sebenarnya
+
+impor spammer_class
+spammer = spammer_class.Spammer ()
+spammer.author =  " P4kL0nc4t "
+spammer.main ()
